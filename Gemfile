140fb530cbac581a63fef9c5ab4ba5395aba3091
t-kashu <146803701+t-kashu@users.noreply.github.com>
2023-11-22 13:03:16 +0900

    devise

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_145058) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_023519) do
   create_table "action_text_rich_texts", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
@@ -54,6 +54,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_145058) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
