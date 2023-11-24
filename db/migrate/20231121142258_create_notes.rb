bc0d01acf01d726ae8e3559d2ae30afc402f9be0
t-kashu <146803701+t-kashu@users.noreply.github.com>
2023-11-24 13:55:37 +0900

    カラム削除

--- db/migrate/20231121142258_create_notes.rb
+++ db/migrate/20231121142258_create_notes.rb
@@ -3,7 +3,6 @@ class CreateNotes < ActiveRecord::Migration[7.0]
     create_table :notes do |t|
       t.string :title
       t.text :content
-      t.references :user,           foreign_key: true
       t.timestamps
     end
   end
