0e624745fb5611af37d19845dc364bdeb90668e6
t-kashu <146803701+t-kashu@users.noreply.github.com>
2023-11-24 12:45:27 +0900

    モデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   has_many :notes, dependent: :destro
+   has_many :notes, dependent: :destroy
 
    validates :name, presence: true
 end
